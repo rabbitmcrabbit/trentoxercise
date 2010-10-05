3a8a95b3634e9ae0c0577839bba4149199104b1b
Michael Specht <micha.specht@gmail.com>
2010-10-05 17:33:28 +0200

    fixed an ugly bug OMG i cann tell y0uu

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import set
+
 def get_row(grid, nr):
     return grid[nr]
 
@@ -10,9 +12,12 @@ def get_box(grid,nr):
     row_idx = [y + (nr/3)*3 for y in range(3)]
     return [grid[y][x] for y in row_idx for x in col_idx]
 
-def is_solution(__):
-    for _ in range(9):
-        if sum(get_row(__, _) != 9) || sum(get_column(__, _) != 9) || sum(get_box(__, _) != 9):
+def is_solution(grid):
+    set9 = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    for i in range(9):
+        if (set(get_row(grid, i)) != set9) || 
+           (set(get_column(grid, i)) != set9) || 
+           (set(get_box(grid, i)) != set9):
             return False
     return True
     
