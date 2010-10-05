8470127fcade9d58e3e873a396ec8cc8fb395f41
Michael Specht <micha.specht@gmail.com>
2010-10-05 17:26:06 +0200

    added is_solution

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -9,4 +9,10 @@ def get_box(grid,nr):
     col_idx = [x + (nr%3)*3 for x in range(3)]
     row_idx = [y + (nr/3)*3 for y in range(3)]
     return [grid[y][x] for y in row_idx for x in col_idx]
+
+def is_solution(__):
+    for _ in range(9):
+        if sum(get_row(__, _) != 9) || sum(get_column(__, _) != 9) || sum(get_box(__, _) != 9):
+            return False
+    return True
     
